63eb77386ea5c80b5f2d2969c1defcac485c4706
nsrosa <nsr@cin.ufpe.br>
2018-09-08 18:08:00 -0300

    Upload of project

---
+++ src/apps/fibonacci/main2/fibonacciserver.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"framework/components/naming/naming"
+	"apps/fibonacci/fibonacciclientproxy"
+	"shared/parameters"
+	"os"
+	"shared/shared"
+	"shared/conf"
+	"executionenvironment/executionenvironment"
+)
+
+func main(){
+
+	// read OS arguments
+	shared.ProcessOSArguments(os.Args[1:])
+
+	// start configuration
+	EE := executionenvironment.ExecutionEnvironment{}
+	EE.Exec(conf.GenerateConf(parameters.DIR_CONF + "/MiddlewareFibonacciServer.conf"),parameters.IS_ADAPTIVE)
+
+	// proxy to naming service
+	namingClientProxy := naming.LocateNaming("localhost",parameters.NAMING_PORT)
+
+	// register
+	fibo := fibonacciclientproxy.FibonacciClientProxy{Host:"localhost",Port:parameters.FIBONACCI_PORT} // TODO
+	namingClientProxy.Register("Fibonacci", fibo)
+	fmt.Println("Fibonacci Server ready at port "+strconv.Itoa(fibo.Port))
+
+	fmt.Scanln()
+	fmt.Println("done")
+}
