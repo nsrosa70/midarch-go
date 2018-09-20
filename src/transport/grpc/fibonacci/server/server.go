64d9ed72f6682dbfe2e05f97e307d32d56c63171
nsrosa <nsr@cin.ufpe.br>
2018-09-20 09:01:18 -0300

    Project configuration

--- src/transport/grpc/fibonacci/server/server.go
+++ src/transport/grpc/fibonacci/server/server.go
@@ -8,8 +8,7 @@ import (
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-        //pb "/Users/nsr/Dropbox/go/software-architecture-v8/src/transport/grpc/fibonacci/fibonacci"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "google.golang.org/grpc/examples/fibonacci/fibonacci"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -17,10 +16,23 @@ const (
 	port = ":50051"
 )
 
+// server is used to implement helloworld.GreeterServer.
 type server struct{}
 
-func (s *server) Fibo(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Fibo " + in.Name}, nil
+// SayHello implements helloworld.GreeterServer
+func (s *server) Fibo(ctx context.Context, in *pb.FiboRequest) (*pb.FiboReply, error) {
+	n := F(in.N)
+	return &pb.FiboReply{N: n}, nil
+}
+
+func F(n int32) int32 {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return F(n-1) + F(n-2)
+	}
 }
 
 func main() {
